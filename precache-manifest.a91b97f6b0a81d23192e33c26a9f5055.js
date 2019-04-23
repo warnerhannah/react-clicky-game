99fb104d232f9f1561ecb9cca941a9425fbfca22
Warnerhannah <warnerhannah1@yahoo.com>
2019-04-23 14:57:18 -0700

    Updates

---
+++ precache-manifest.a91b97f6b0a81d23192e33c26a9f5055.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf44b7743a70a5d393021938c477fb3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e5d11b41363800aa08",
+    "url": "/static/css/main.54961816.chunk.css"
+  },
+  {
+    "revision": "3cd8084af2b08306ce6c",
+    "url": "/static/js/2.fa4c51f4.chunk.js"
+  },
+  {
+    "revision": "16e5d11b41363800aa08",
+    "url": "/static/js/main.9c076c39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
